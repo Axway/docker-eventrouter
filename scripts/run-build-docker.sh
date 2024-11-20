efb914b1847b6088a830909b92bda0eeeed0ac7b
Flavia Tovo <ftovo@axway.com>
2024-11-20 14:04:33 +0000

    SER-394 - Test if General Terms and Conditions are accepted

--- scripts/run-build-docker.sh
+++ scripts/run-build-docker.sh
@@ -10,7 +10,7 @@ CONTAINER=$COMPOSE_PROJECT_NAME-$NAME
 run() {
     docker rm -f $CONTAINER || true
     docker build -f docker/Dockerfile.glibc -t event_router_build_glibc .
-    docker run --name $CONTAINER event_router_build_glibc $NAME version
+    docker run --env ACCEPT_GENERAL_CONDITIONS=YES --name $CONTAINER event_router_build_glibc $NAME version
     docker cp $CONTAINER:/usr/bin/$NAME .
     docker rm -f $CONTAINER || true 
 }
